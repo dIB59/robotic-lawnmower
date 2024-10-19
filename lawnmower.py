767dad22f03bf97ba74a4b47e046f944d2867d8f
Ibrahim <Ibrahim@ebs.net.pk>
2024-10-19 12:06:30 +0200

    fix: collisions

--- lawnmower.py
+++ lawnmower.py
@@ -3,7 +3,8 @@ from grid import get_obstacles
 
 
 class Lawnmower:
-    def __init__(self, x=0.0, y=0.0, v_x=0.0, v_y=0.0, grid_len_x=0, grid_len_y=0, obstacles: list[int] = None):
+    def __init__(self, x=0.0, y=0.0, v_x=0.0, v_y=0.0, grid_len_x=0, grid_len_y=0,
+                 obstacles: list[tuple[int, int]] = None):
         self.x = x
         self.y = y
 
@@ -19,23 +20,22 @@ class Lawnmower:
         #     raise AssertionError
 
     def move(self, dt: int = 1):
-        self.x += self.v_x * dt
-        self.y += self.v_y * dt
+        new_x = self.x + self.v_x * dt
+        new_y = self.y + self.v_y * dt
 
-        if 2 <= self.x <= 3 and 2 <= self.y <= 4:
-            self.x = self.x - self.v_x * dt
+        if self.is_in_obstacle(new_x, self.y):
             self.v_x *= -1
+        else:
+            self.x = new_x
 
-        if self.x > self.max_x:
-            self.v_x *= -1
-
-        if self.y > self.max_y:
+        if self.is_in_obstacle(self.x, new_y):
             self.v_y *= -1
+        else:
+            self.y = new_y
 
-        if self.x < 0:
+        if self.x > self.max_x or self.x < 0:
             self.v_x *= -1
-
-        if self.y < 0:
+        if self.y > self.max_y or self.y < 0:
             self.v_y *= -1
 
     def get_pos(self) -> (float, float):
@@ -56,3 +56,10 @@ class Lawnmower:
 
         # Show the plot
         plt.show()
+
+    def is_in_obstacle(self, new_x, new_y):
+        # Check if the new position is in an obstacle
+        for ox, oy in self.obstacles:
+            if ox <= new_x <= ox + 1 and oy <= new_y <= oy + 1:
+                return True
+        return False
